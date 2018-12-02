c64d0654ef77eb99ebbc9b4705d047c31630a685
Lucyna Aleksandra <lucy@lucynaaleksandra.com>
2018-12-01 20:24:41 -0500

    initial commit

---
+++ src/components/clients/EditClient.js
@@ -0,0 +1,154 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import PropTypes from 'prop-types'
+
+import Spinner from '../layout/Spinner'
+
+
+class EditClient extends Component {
+  constructor(props) {
+    super(props) 
+
+    this.firstNameInput = React.createRef()
+    this.lastNameInput = React.createRef()
+    this.emailInput = React.createRef()
+    this.phoneInput = React.createRef()
+    this.balanceInput = React.createRef()
+  }
+
+  onSubmit = e => {
+    e.preventDefault()
+
+    const { client, firestore, history } = this.props
+
+    // updated client
+    const updClient = {
+      firstName: this.firstNameInput.current.value, 
+      lastName: this.lastNameInput.current.value, 
+      email: this.emailInput.current.value, 
+      phone: this.phoneInput.current.value, 
+      // if it's empty, submit as 0 value
+      balance: 
+        this.balanceInput.current.value === "" 
+          ? 0 
+          : this.balanceInput.current.value
+    }
+
+    // update client inn firestore
+    firestore
+      .update({ collection: "clients", doc: client.id}, updClient)
+      .then(() => history.push("/"))
+  }
+
+  render() {
+    const { client } = this.props
+    const { disableBalanceOnEdit} = this.props.settings
+
+    if (client) {
+      return (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <Link to="/" className="btn btn-link" >
+                <i className="fas fa-arrow-circle-left" />{'  '} Back to Dashboard
+            </Link>
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="card-header">Add Client</div>
+            <div className="card-body">
+              <form onSubmit={this.onSubmit} >
+                <div className="form-group">
+                  <label htmlFor="firstName">First Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="firstName"
+                    minLength="2"
+                    onChange={this.onChange}
+                    defaultValue={client.firstName}
+                    ref={this.firstNameInput}
+                    required
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="lastName">Last Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="lastName"
+                    minLength="2"
+                    onChange={this.onChange}
+                    defaultValue={client.lastName}
+                    ref={this.lastNameInput}
+                    required
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    name="email"
+                    onChange={this.onChange}
+                    defaultValue={client.email}
+                    ref={this.emailInput}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="phone">Phone</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="phone"
+                    minLength="10"
+                    onChange={this.onChange}
+                    defaultValue={client.phone}
+                    ref={this.phoneInput}
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="balance">Balance</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="balance"
+                    onChange={this.onChange}
+                    defaultValue={client.balance}
+                    ref={this.balanceInput}
+                    disabled={disableBalanceOnEdit}
+                  />
+                </div>
+                <input type="submit" value="submit" className="btn btn-primary btn-block" />
+
+              </form>
+            </div>
+          </div>
+        </div>
+      )
+    } else {
+      return <Spinner />
+    }
+  }
+}
+
+EditClient.propTypes = {
+  firestore: PropTypes.object.isRequired
+}
+
+export default compose(
+  firestoreConnect(props => [
+    { collection: "clients", storeAs: "client", doc: props.match.params.id }
+  ]),
+  connect(({ firestore: { ordered }, settings }, props) => ({
+    client: ordered.client && ordered.client[0],
+    settings
+  }))
+)(EditClient)
---
+++ src/store.js
@@ -0,0 +1,81 @@
+import { createStore, combineReducers, compose } from 'redux'
+import firebase from 'firebase'
+import 'firebase/firestore'
+import { reactReduxFirebase, firebaseReducer } from 'react-redux-firebase'
+import { reduxFirestore, firestoreReducer } from 'redux-firestore'
+import notifyReducer from './reducers/notifyReducer'
+import settingsReducer from './reducers/settingsReducer'
+
+const API_KEY = process.env.REACT_APP_API_KEY
+const AUTH_DOMAINN = process.env.REACT_APP_AUTH_DOMAINN
+const DATABASE_URL = process.env.REACT_APP_DATABASE_URL
+const PROJECT_ID = process.env.REACT_APP_PROJECT_ID
+const STORAGE_BUCKET = process.env.REACT_APP_STORAGE_BUCKET
+const MESSAGING_SENDER_ID = process.env.REACT_APP_MESSAGING_SENDER_ID
+
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAINN,
+  databaseURL: DATABASE_URL,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID
+}
+
+// react-redux firebase config
+const rrfConfig = {
+  userProfile: 'users',
+  useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+}
+
+// init firebase instance
+firebase.initializeApp(firebaseConfig)
+// init firestore
+const firestore = firebase.firestore()
+const settings = {/* your settings... */ timestampsInSnapshots: true };
+firestore.settings(settings);
+
+
+// Add reactReduxFirebase enhancer when making store creator
+const createStoreWithFirebase = compose(
+  reactReduxFirebase(firebase, rrfConfig), // firebase instance as first argument
+  reduxFirestore(firebase) // <- needed if using firestore
+)(createStore)
+
+
+// Add firebase to reducers
+const rootReducer = combineReducers({
+  firebase: firebaseReducer,
+  firestore: firestoreReducer, // <- needed if using firestore
+  notify: notifyReducer,
+  settings: settingsReducer
+})
+
+// check for settings in localStorage
+if (localStorage.getItem("settings") === null) {
+  // default settings
+  const defaultSettings = {
+    disableBalanceOnAdd: true,
+    disableBalanceOnEdit: false,
+    allowRegistration: false  
+  } 
+
+  // set to localStorage
+  localStorage.setItem("settings", JSON.stringify(defaultSettings))
+}
+
+
+// create initial state
+const initialState = { settings: JSON.parse(localStorage.getItem("settings")) }
+
+// create store
+const store = createStoreWithFirebase(
+  rootReducer,
+  initialState,
+  compose(
+    reactReduxFirebase(firebase),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+)
+
+export default store
